be42c5d523c485432e558108b08c4f3dd1b421c8
Andreas <andreashellesvik@hotmail.com>
2025-07-05 15:25:40 +0200

    Setup vite and ts with tanstack start

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import 'dotenv/config'
+import { defineConfig } from 'vite'
+import tsConfigPaths from 'vite-tsconfig-paths'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+
+const port = parseInt(process.env.PORT || '3000')
+
+export default defineConfig({
+  server: {
+    port: port,
+  },
+  plugins: [tsConfigPaths(), tanstackStart()],
+})
