0d57cd0277eb9d52d0f2a9ab2b5bd2bacc88a483
Andreas <andreashellesvik@hotmail.com>
2025-07-17 06:40:46 +0200

    Add location as dependency

--- src/components/chat/Message.tsx
+++ src/components/chat/Message.tsx
@@ -61,127 +61,145 @@ export default function Message() {
     [appendMessage, setError]
   )
 
-  const handleStreamSubmit = async (e?: React.FormEvent<HTMLFormElement>) => {
-    e?.preventDefault()
+  const handleStreamSubmit = useCallback(
+    async (e?: React.FormEvent<HTMLFormElement>) => {
+      e?.preventDefault()
 
-    const formData = new FormData(formRef.current!)
-    let isNew = true
-    let chatId: string = ''
+      const formData = new FormData(formRef.current!)
+      let isNew = true
+      let chatId: string = ''
 
-    let ctrl = new AbortController()
-    if (location.pathname.includes('/chat/')) {
-      chatId = z.uuidv4().parse(location.pathname.split('/').pop())
-      formData.set('chatId', chatId)
-      isNew = false
-    } else {
+      let ctrl = new AbortController()
+      if (location.pathname.includes('/chat/')) {
+        chatId = z.uuidv4().parse(location.pathname.split('/').pop())
+        formData.set('chatId', chatId)
+        isNew = false
+      } else {
+        try {
+          chatId = await generateChatId({
+            data: {
+              userId: '2f643d7f-ffe9-4b0a-87ba-40198838805a',
+            },
+            signal: ctrl.signal,
+          })
+        } catch (error) {
+          setError(
+            error instanceof Error
+              ? error.message
+              : 'Failed to generate message ID'
+          )
+          console.error('Error generating message ID:', error)
+          return
+        }
+        formData.set('chatId', chatId)
+        navigate({ to: `/chat/${chatId}`, replace: true })
+      }
+      const modelNameResult = modelNameSchema.safeParse(formData.get('model'))
+      if (!modelNameResult.success) {
+        setError(modelNameResult.error.issues[0].message)
+        console.error('modelNameResult:', modelNameResult.error)
+        return
+      }
+      formData.set('model', modelNameResult.data)
+
+      const messageResult = messageSchema.safeParse(formData.get('message'))
+      if (!messageResult.success) {
+        setError(messageResult.error.issues[0].message)
+        console.error('messageResult:', messageResult.error)
+        return
+      }
+      addMessage(messageResult.data)
+      formRef.current?.reset()
+      setIsChatLoading(true)
+      setIsSubmitting(true)
+      setCanSubmit(false)
+      setError('')
+      let reader: ReadableStreamDefaultReader | null = null
+      let previousTexts = []
+      let isSummary = false
+
       try {
-        chatId = await generateChatId({
-          data: {
-            userId: '2f643d7f-ffe9-4b0a-87ba-40198838805a',
-          },
+        const response = await postMessage({
           signal: ctrl.signal,
+          data: formData,
         })
-      } catch (error) {
-        setError(
-          error instanceof Error
-            ? error.message
-            : 'Failed to generate message ID'
-        )
-        console.error('Error generating message ID:', error)
-        return
-      }
-      formData.set('chatId', chatId)
-      navigate({ to: `/chat/${chatId}`, replace: true })
-    }
-    const modelNameResult = modelNameSchema.safeParse(formData.get('model'))
-    if (!modelNameResult.success) {
-      setError(modelNameResult.error.issues[0].message)
-      console.error('modelNameResult:', modelNameResult.error)
-      return
-    }
-    formData.set('model', modelNameResult.data)
+        if (!response.ok || !response.body) {
+          setError('Failed to connect to stream')
+          return
+        }
+        reader = response.body.getReader()
+        const decoder = new TextDecoder()
+        let done = false
 
-    const messageResult = messageSchema.safeParse(formData.get('message'))
-    if (!messageResult.success) {
-      setError(messageResult.error.issues[0].message)
-      console.error('messageResult:', messageResult.error)
-      return
-    }
-    addMessage(messageResult.data)
-    formRef.current?.reset()
-    setIsChatLoading(true)
-    setIsSubmitting(true)
-    setCanSubmit(false)
-    setError('')
-    let reader: ReadableStreamDefaultReader | null = null
-    let previousTexts = []
-    let isSummary = false
+        while (!done) {
+          const { value, done: streamDone } = await reader.read()
+          done = streamDone
+          if (value) {
+            const textChunk = decoder.decode(value, { stream: !done })
+            if (!isSummary) {
+              handleTextChunk(textChunk)
+              console.log('textChunk:', textChunk)
+            }
+            if (isNew) {
+              // Summary should not be longer than n chunks
+              if (previousTexts.length >= 10) {
+                previousTexts.shift()
+              }
+              console.log('previousTexts:', previousTexts)
+              previousTexts.push(textChunk)
+              if (
+                !isSummary &&
+                previousTexts.join('').includes(SUMMARY_START)
+              ) {
+                isSummary = true
+                stripSummary()
+              }
+              console.log('isSummary:', isSummary)
+            }
+          }
+        }
 
-    try {
-      const response = await postMessage({
-        signal: ctrl.signal,
-        data: formData,
-      })
-      if (!response.ok || !response.body) {
-        setError('Failed to connect to stream')
-        return
-      }
-      reader = response.body.getReader()
-      const decoder = new TextDecoder()
-      let done = false
-
-      while (!done) {
-        const { value, done: streamDone } = await reader.read()
-        done = streamDone
-        if (value) {
-          const textChunk = decoder.decode(value, { stream: !done })
+        // flush any remaining data
+        const chunkRemains = decoder.decode()
+        if (chunkRemains) {
+          console.log('chunkRemains:', chunkRemains)
+          previousTexts.push(chunkRemains)
           if (!isSummary) {
-            handleTextChunk(textChunk)
-            console.log('textChunk:', textChunk)
-          }
-          if (isNew) {
-            // Summary should not be longer than n chunks
-            if (previousTexts.length >= 10) {
-              previousTexts.shift()
-            }
-            console.log('previousTexts:', previousTexts)
-            previousTexts.push(textChunk)
-            if (!isSummary && previousTexts.join('').includes(SUMMARY_START)) {
-              isSummary = true
-              stripSummary()
-            }
-            console.log('isSummary:', isSummary)
+            handleTextChunk(chunkRemains)
           }
         }
-      }
 
-      // flush any remaining data
-      const chunkRemains = decoder.decode()
-      if (chunkRemains) {
-        console.log('chunkRemains:', chunkRemains)
-        previousTexts.push(chunkRemains)
-        if (!isSummary) {
-          handleTextChunk(chunkRemains)
+        if (isSummary) {
+          const [_, summary] = extractTextAndSummary(previousTexts.join(''))
+          setName(summary)
+          console.log('add to chat list', summary)
+          addToChatList({ chatId, name: summary })
         }
+      } catch (err) {
+        setError(
+          'Streaming error: ' +
+            (err instanceof Error ? err.message : String(err))
+        )
+      } finally {
+        reader?.releaseLock()
+        setIsChatLoading(false)
+        setIsSubmitting(false)
+        setCanSubmit(true)
       }
-
-      if (isSummary) {
-        const [_, summary] = extractTextAndSummary(previousTexts.join(''))
-        setName(summary)
-        console.log('add to chat list', summary)
-        addToChatList({ chatId, name: summary })
-      }
-    } catch (err) {
-      setError(
-        'Streaming error: ' + (err instanceof Error ? err.message : String(err))
-      )
-    } finally {
-      reader?.releaseLock()
-      setIsChatLoading(false)
-      setIsSubmitting(false)
-      setCanSubmit(true)
-    }
-  }
+    },
+    [
+      location,
+      navigate,
+      appendMessage,
+      addMessage,
+      setIsChatLoading,
+      setIsSubmitting,
+      setCanSubmit,
+      setError,
+      addToChatList,
+      stripSummary,
+    ]
+  )
 
   return (
     <form ref={formRef} onSubmit={handleStreamSubmit}>
